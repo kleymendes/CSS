a63915a7087c82cf1b0b115a46180277207f61e5
Kley Silva <kleymendes2@gmail.com.br>
2021-02-06 13:22:29 -0300

    Primeiro Commit

---
+++ JS/index.js
@@ -0,0 +1,34 @@
+var nome = "Kleidilson Silva";
+var cargo = "Develop";
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomenoHtml(nome) {
+    nomeHtml.innerHTML = nome
+}
+
+function colocarCargoNoHtml(cargo) {
+    cargoHtml.innerHTML = cargo
+}
+
+function logarNome() {
+    console.log(nome);
+}
+
+function clickNoProjetos() {
+    console.log("clicou no botão projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clickNoSobre() {
+    console.log("clicou no botão sobre");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+
+colocarNomenoHtml(nome);
+colocarCargoNoHtml(cargo);
